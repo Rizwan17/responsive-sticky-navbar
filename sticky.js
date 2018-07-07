05dbee1fb7bd5093b70fd3f914ad6a6c2344edc5
Rizwan Khan <rizwankhan.august16@gmail.com>
2018-07-07 13:19:30 +0530

    Sticky Responsive Navbar

---
+++ sticky.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $(".sub-header").addClass('active');
+        }else{
+            $(".sub-header").removeClass('active');
+        }
+    });
+});
